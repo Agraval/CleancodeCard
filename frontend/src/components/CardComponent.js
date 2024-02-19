84e40b6cccbf4638f5aa73db96fbd57c72138565
agraval <alexandre.samson78@gmail.com>
2024-02-19 20:31:07 +0100

    Get_Card

--- frontend/src/components/CardComponent.js
+++ frontend/src/components/CardComponent.js
@@ -1,17 +1,14 @@
-import React from 'react';
-
 function CardComponent({ card, onAnswer }) {
     const handleAnswer = (isCorrect) => {
         onAnswer(card.id, isCorrect);
     };
 
     return (
-        <div>
-            <div>{card.question}</div>
+        <div className="card">
+            <div>Question: {card.question}</div>
+            <div>Réponse: {card.answer}</div> {/* Afficher la réponse pourrait être optionnel selon les besoins de l'application */}
             <button onClick={() => handleAnswer(true)}>Correct</button>
             <button onClick={() => handleAnswer(false)}>Incorrect</button>
         </div>
     );
 }
-
-export default CardComponent;
