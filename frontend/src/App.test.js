e003c2e1e2263c7985d7b1af77f29623511e1b18
CamilleBorra <cborra@hotmail.fr>
2024-02-20 19:46:15 +0100

    add front test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,22 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders App component with navigation links', () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+
+  // Vérifie que le texte "Learning Cards" est présent dans l'AppBar
+  expect(screen.getByText('Learning Cards')).toBeInTheDocument();
+
+  // Vérifie que les liens "Add Card" et "Quiz" sont présents dans l'AppBar
+  expect(screen.getByText(/add card/i).closest('a')).toBeInTheDocument();
+  expect(screen.getByText(/quiz/i).closest('a')).toBeInTheDocument();
 });
+
+
